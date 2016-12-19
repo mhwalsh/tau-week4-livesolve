fe2a64965da43e90a743679e543a312ec0fee560
Millie Walsh <millie@primeacademy.io>
2016-12-18 22:13:18 -0600

    styles and delete button

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
   console.log('doc ready');
   getTasks();
   $('#createTask').on('click', createTask);
+  $('#taskDiv').on('click', '.deleteTask', deleteTask);
 });
 
 // ajax functions
@@ -42,12 +43,31 @@ function createTask(){
   });
 }
 
+
+function deleteTask(){
+  console.log('called delete task');
+  var id = $(this).parent().data('id');
+  console.log('id ->', id);
+
+  $.ajax({
+    type: 'DELETE',
+    url: '/task/' + id,
+    success: function() {
+      console.log(response);
+      getTasks();
+    }
+  });
+}
+
 function appendTasks(tasks) {
   console.log('called appendTasks');
   $('#taskDiv').empty();
-  
+
   tasks.forEach(function(task) {
     console.log('task ->', task);
-    $('#taskDiv').append("<div>" + task.name +"</div>");
+    $('#taskDiv').append('<div class="task">' + task.name +'</div>');
+    $el = $('#taskDiv').children().last();
+    $el.data('id', task.id);
+    $el.append('<button class="deleteTask">Delete</button>');
   });
 }
