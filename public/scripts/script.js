9ba44097516f9d7ad24db43aacdb76c26eab0ae6
Millie Walsh <millie@primeacademy.io>
2016-12-18 20:19:40 -0600

    first pass at dom append

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,4 +2,26 @@ console.log('sourced!');
 
 $(document).ready(function() {
   console.log('doc ready');
+  getTasks();
 });
+
+// ajax functions
+function getTasks(){
+  // $.get('/task', appendTasks());
+  $.ajax({
+    type: 'GET',
+    url: '/task',
+    success: function(response) {
+      console.log('response ->', response);
+      appendTasks(response);
+    }
+  });
+}
+
+function appendTasks(tasks) {
+  console.log('called appendTasks');
+  tasks.forEach(function(task) {
+    console.log('task ->', task);
+    $('#taskDiv').append("<div>" + task.name +"</div>");
+  });
+}
