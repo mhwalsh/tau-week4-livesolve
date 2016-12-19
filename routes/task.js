dc6df661ed339889dafbdfc00756245993c806a4
Millie Walsh <millie@primeacademy.io>
2016-12-18 20:11:08 -0600

    complete get tasks route

--- routes/task.js
+++ routes/task.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var pg = require('pg');
+
+var connectionString = 'postgres://localhost:5432/tau-tasks';
 
 router.get('/', function(req, res) {
   console.log('in get tasks route');
-  res.sendStatus(200);
+
+  pg.connect(connectionString, function(err, client, done) {
+    if(err){
+      console.log(err);
+      res.sendStatus(500);
+    }
+
+    client.query("SELECT * FROM task;", function(err, result) {
+      done(); //close connection to db
+
+      if(err){
+        console.log(err);
+        res.sendStatus(500);
+      }
+
+      console.log('results -> ', result.rows);
+      res.send(result.rows);
+    });
+  });
 });
 
 module.exports = router;
